aed04e429b060efc0541828b3e90ee46a4dc7680
Raphael Kuster <raphael.kuster@mia-systems.at>
2019-04-10 14:19:14 +0200

    Init

---
+++ MoveFilesFromSubFolder/MoveFilesFromSubFolder/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MoveFilesFromSubFolder
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPath;
+            string outputPath;
+            bool detailedLogging;
+            bool move;
+
+            switch (args.Length)
+            {
+                case 0:
+                    inputPath = Environment.CurrentDirectory;
+
+                    Console.WriteLine("Not Outputpath specified, please input one now:");
+
+                    outputPath = Console.ReadLine();
+                    detailedLogging = false;
+                    move = false;
+                    break;
+                case 1:
+                    inputPath = args[0];
+
+                    Console.WriteLine("Not Outputpath specified, please input one now:");
+
+                    outputPath = Console.ReadLine();
+                    detailedLogging = false;
+                    move = false;
+                    break;
+                case 2:
+                    inputPath = args[0];
+                    outputPath = args[1];
+
+                    detailedLogging = false;
+                    move = false;
+                    break;
+                case 3:
+                    inputPath = args[0];
+                    outputPath = args[1];
+                    bool.TryParse(args[2], out detailedLogging);
+
+                    move = false;
+                    break;
+                case 4:
+                    inputPath = args[0];
+                    outputPath = args[1];
+                    bool.TryParse(args[2], out detailedLogging);
+                    bool.TryParse(args[3], out move);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(@"Too many arguments");
+            }
+
+            List<FileInfo> inputFiles = MoveAllFiles(inputPath);
+
+            foreach (FileInfo inputFile in inputFiles)
+            {
+                try
+                {
+                    if (move)
+                    {
+                        File.Move(inputFile.FullName, Path.Combine(outputPath, inputFile.Name));
+                    }
+                    else
+                    {
+                        File.Copy(inputFile.FullName, Path.Combine(outputPath, inputFile.Name), true);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    break;
+                }
+
+                string filename = detailedLogging ? inputFile.FullName : inputFile.Name;
+                string movedOrCopied = move ? "moved" : "copied";
+                Console.WriteLine($"File {filename} {movedOrCopied}");
+            }
+        }
+
+        private static List<FileInfo> MoveAllFiles(string inputPath)
+        {
+            var outputFiles = new List<FileInfo>();
+            var directories = Directory.GetDirectories(inputPath);
+            foreach (string directory in directories)
+            {
+                outputFiles.AddRange(MoveAllFiles(directory));
+            }
+
+            outputFiles.AddRange(Directory.GetFiles(inputPath).Select(file => new FileInfo(file)));
+
+            return outputFiles;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # MoveFilesFromSubfolder
 Moves all files from a folder including all subfolders to another folder
+
+## Usage
+```
+MoveFilesToSubFolder.exe <InputPath> <OutputPath> <DetailedLogging> <MoveOrCopy>
+```
+### Example
+```
+MoveFilesToSubFolder.exe "C:\temp" "C:\tempWithAllFiles" true true
+```
+
+### Options
+
+#### ```<InputPath>```
+Path with the files in subfolders, if empty the path where the exe is is used.
+
+#### ```<OutputPath> ```
+Path where all the files should be placed.
+
+#### ```<DetailedLogging> ```
+bool value (true/false) 
+write the whole path to the console.
+
+#### ```<MoveOrCopy>```
+bool value (true/false) 
+move or copy the files.
+
+## Used Nugets
